abf2dd71e3585334dce6d1c3d7c9128b65875aa1
jes-rod <104109970+jes-rod@users.noreply.github.com>
2022-06-28 15:56:36 -0600

    Update SearchBar.js
    Removed "Check" button used for internal testing

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -29,8 +29,7 @@ class SearchBar extends React.Component{
       <div className="SearchBar">
         <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange}/>
         <button className="SearchButton" onClick={this.search}>SEARCH</button>
-        <button className="SearchButton" onClick={this.check}>CHECK</button>
-      </div>
+        </div>
     )
   }
   
